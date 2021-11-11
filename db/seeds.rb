4c0f4a8cb18393cb8199b251b18b40675a82e9c3
Alena Bauer <alena.a.bauer@gmail.com>
2021-11-11 14:38:11 +0100

    add lists and bookmarcs mvc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+require 'dotenv'
+
+Dotenv.load('../.env')
+API_KEY = ENV['TMDB_API']
+
+url = "https://api.themoviedb.org/3/movie/top_rated?api_key=#{API_KEY}&language=en-US&page=11"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)['results']
+
+puts 'Creating movies...'
+
+movies.each do |movie|
+  puts "Adding #{movie['original_title']}"
+  Movie.create!({
+    title: movie['original_title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie['vote_average']
+  })
+end
+
+puts 'Creating lists...'
+List.create!(name: 'Favourite')
+List.create!(name: 'Classic')
+List.create!(name: 'Rewatch')
+
+puts 'Finished!'
